30c0032dc67a6fa1ac766346347c3cd6101af712
Lou .A <loualicea@hotmail.com>
2023-03-12 15:25:08 -0400

    created user controller and added structure

---
+++ Main/controllers/userController.js
@@ -0,0 +1,76 @@
+const User = require('../models/User');
+
+module.exports = {
+  
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  
+  createUser(req, res) {
+    User.create(req.body)
+      .then((userData) => res.json(userData))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user found with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+ 
+  updateUser(req, res) {
+    User.updateOne(
+      {
+        "userId": req.params.userId
+      },
+      {
+        $set: { "username": req.body.username }
+      }
+    )
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  
+  deleteUser(req, res) {
+    User.deleteOne({ 'userId': req.params.userId })
+      .then(() => res.json({ message: 'User deleted.' }))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  
+  addFriend(req, res) {
+    User.updateOne(
+      {
+        'userId': req.params.userId
+      },
+      {
+        $addToSet: { friends: req.params.friendId }
+      }
+    )
+      .then(() => res.json({ message: 'Added Friend.' }))
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteFriend(req, res) {
+    User.deleteOne(
+      {
+        'userId': req.params.userId
+      },
+      {
+        $pull: { friends: req.params.friendId }
+      }
+    )
+      .then(() => res.json({ message: 'Removed Friend.' }))
+      .catch((err) => res.status(500).json(err));
+  },
+}
